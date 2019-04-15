3777e12edbc5cb1851eac47b8ab8006595f57de0
MatTerra <mateus.b.s.terra@gmail.com>
2019-04-15 00:06:12 -0300

    Iniciada adaptação para os requisitos

--- src/camera_calibration.py
+++ src/camera_calibration.py
@@ -3,7 +3,6 @@
 # General imports
 import numpy as np
 import cv2
-import json
 import glob
 
 # MACROS
@@ -21,13 +20,13 @@ def fill_data(images, objpoints, imgpoints, debug=True, hcenters=H_CENTERS, vcen
     criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 30, 0.001)
 
     # prepare object points, like (0,0,0), (1,0,0), (2,0,0) ....,(6,5,0)
-    objp = np.zeros((H_CENTERS * V_CENTERS,3), np.float32)
-    objp[:,:2] = np.mgrid[0:H_CENTERS, 0:V_CENTERS].T.reshape(-1,2)
+    objp = np.zeros((H_CENTERS * V_CENTERS, 3), np.float32)
+    objp[:, :2] = np.mgrid[0:H_CENTERS, 0:V_CENTERS].T.reshape(-1, 2)
 
     for fname in images:
-        frame = cv2.imread(fname)
-        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-
+        gray = cv2.imread(fname, cv2.IMREAD_GRAYSCALE)
+        if debug:
+            img = cv2.cvtColor(gray, cv2.COLOR_GRAY2BGR)
         # Find the chessboard centers
         corners_were_found, corners = cv2.findChessboardCorners(gray, (H_CENTERS, V_CENTERS), None)
 
@@ -37,24 +36,24 @@ def fill_data(images, objpoints, imgpoints, debug=True, hcenters=H_CENTERS, vcen
             cv2.cornerSubPix(gray, corners, (11,11), (-1,-1), criteria)
             imgpoints.append(corners)
 
-            if debug == True:
-                cv2.drawChessboardCorners(frame, (H_CENTERS, V_CENTERS), corners, corners_were_found)
+            if debug:
+                cv2.drawChessboardCorners(img, (H_CENTERS, V_CENTERS), corners, corners_were_found)
 
-        if debug == True:
-            cv2.imshow('window', frame)
+        if debug:
+            cv2.imshow('window', img)
             cv2.waitKey(100)
 
-    if debug == True:
+    if debug:
         cv2.destroyWindow('window')
 
-def store_matrices(mat_x, mat_y, file_name):
-    params= {}
-    params['matrix_x'] = mat_x.tolist()
-    params['matrix_y'] = mat_y.tolist()
 
-    file = open(file_name, "w+")
-    json.dump(params, file)
-    file.close()
+def store_matrices(intrisics, distortion, n=0):
+    mtx_F = cv2.FileStorage("intrisics_"+str(n)+".xml", cv2.FILE_STORAGE_WRITE)
+    mtx_F.write("F", intrisics)
+    mtx_F.release()
+    mtx_dist = cv2.FileStorage("distortion_"+str(n)+".xml", cv2.FILE_STORAGE_WRITE)
+    mtx_dist.write("DIST", distortion)
+    mtx_dist.release()
 
 
 def get_error(objp, imgpoints, rvecs, tvecs, mtx, distortion_vector):
@@ -64,6 +63,7 @@ def get_error(objp, imgpoints, rvecs, tvecs, mtx, distortion_vector):
         error = cv2.norm(imgpoints[i],imgpoints2, cv2.NORM_L2)/len(imgpoints2)
         mean_error += error
     return mean_error/len(objpoints)
+
 
 def show_result(mapx, mapy, cam_id=CAMERA_ID):
     cap = cv2.VideoCapture(cam_id)
@@ -82,45 +82,102 @@ def show_result(mapx, mapy, cam_id=CAMERA_ID):
     cv2.destroyWindow('Undistorted')
 
 
+def capture_five_pat(x):
+    cap = cv2.VideoCapture(0)
+    ret, frame = cap.read()
+    seq = x
+    while ret and x < (seq+5):
+        ret, frame = cap.read()
+        cv2.imshow(str(seq/5)+' sequência de calibração', frame)
+        key = cv2.waitKey(1)
+        if key == ord('c'):
+            corners_were_found, corners = cv2.findChessboardCorners(cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY), (H_CENTERS, V_CENTERS), None)
+            if corners_were_found:
+                nome = ".jpg"
+                cv2.imwrite(str(x) + nome, frame)
+                x = x + 1
+        if key == ord('q'):
+            break
+    cv2.destroyWindow(str(seq/5)+' sequência de calibração')
+    cap.release()
+
+def get_num(elem):
+    return int(elem.replace(".jpg", ""))
+
+
 if __name__ == "__main__":
 
-    # Lists to store object points and image points from all the images.
-    objpoints = []  # 3d point in real world space
-    imgpoints = []  # 2d points in image plane.
+    rets = []
+    mtxs = []
+    distortion_vectors = []
+    rvecss = []
+    tvecss = []
 
     cap = cv2.VideoCapture(0)
     ret, frame = cap.read()
-    x = 0
-    while ret:
-        ret, frame = cap.read()
-        cv2.imshow('frame', frame)
-        key = cv2.waitKey(1)
-        if key == ord('c'):
-            nome = ".jpg"
-            cv2.imwrite(str(x)+nome, frame)
-            x = x+1
-        if key == ord('q'):
-            break
+    nome = ".jpg"
+    cv2.imwrite("tamanho" + nome, frame)
     cap.release()
-    images = glob.glob('*.jpg')
-    gray = cv2.cvtColor(cv2.imread(images[0]), cv2.COLOR_BGR2GRAY)
-
-    fill_data(images, objpoints, imgpoints)
-
-    print("Calculating correction matrices")
-    ret, mtx, distortion_vector, rvecs, tvecs = cv2.calibrateCamera(objpoints, imgpoints, gray.shape[::-1], None, None)
-
-    h, w = gray.shape
+    h, w = cv2.imread("tamanho.jpg", cv2.IMREAD_GRAYSCALE).shape
     # output image size after the lens correction
     nh, nw = int(SCALE * h), int(SCALE * w)
 
-    newcameramtx, roi=cv2.getOptimalNewCameraMatrix(mtx, distortion_vector, (w,h), 1, (nw, nh))
-    # get undistorion maps
-    mapx, mapy = cv2.initUndistortRectifyMap(mtx, distortion_vector, None, newcameramtx, (nw, nh), 5)
+    intrisics_matrices_names = glob.glob("intrisics_*.xml")
+    distortion_vectors_names = glob.glob("distortion_*.xml")
+    if len(intrisics_matrices_names) > 4 and len(distortion_vectors_names) > 4:
+        print("have parameters")
+        for x in range(len(intrisics_matrices_names)):
+            file_mtx = cv2.FileStorage("intrisics_"+str(x)+".xml", cv2.FILE_STORAGE_READ)
+            matrix = file_mtx.getNode("F").mat()
+            file_mtx.release()
+            if(len(mtxs)==0):
+                mtxs.append(matrix)
+            else:
+                mtxs[0] += matrix
+        mtxs[0] /= len(intrisics_matrices_names)
+        for x in range(len(distortion_vectors_names)):
+            file_mtx = cv2.FileStorage("distortion_"+str(x)+".xml", cv2.FILE_STORAGE_READ)
+            matrix = file_mtx.getNode("DIST").mat()
+            file_mtx.release()
+            if (len(distortion_vectors) == 0):
+                distortion_vectors.append(matrix)
+            else:
+                distortion_vectors[0] += matrix
+        distortion_vectors[0] /= len(distortion_vectors_names)
 
-    store_matrices(mapx, mapy, "camera_matrices.json")
+    else:
+        # Lists to store object points and image points from all the images.
+        for x in range(0, 5):
+            objpoints = []  # 3d point in real world space
+            imgpoints = []  # 2d points in image plane.
+            capture_five_pat(5*x)
+            images = glob.glob('[0-9]*.jpg')
+            images.sort(key=get_num)
+            images = images[(5*x):(5*(x+1)):1]
+            fill_data(images, objpoints, imgpoints, False)
 
-    print("STD Error:", get_error(objpoints, imgpoints, rvecs, tvecs, mtx, distortion_vector))
+            print("Calculating correction matrices")
+            ret, mtx, distortion_vector, rvecs, tvecs = \
+                cv2.calibrateCamera(objpoints,
+                                    imgpoints,
+                                    cv2.imread(images[0], cv2.IMREAD_GRAYSCALE).shape[::-1],
+                                    None,
+                                    None)
 
+            rets.append(ret)
+            mtxs.append(mtx)
+            distortion_vectors.append(distortion_vector)
+            rvecss.append(rvecs)
+            tvecss.append(tvecs)
+
+            print(mtx)
+            # get undistorion maps
+            newcameramtx, roi = cv2.getOptimalNewCameraMatrix(mtx, distortion_vector, (w, h), 1, (nw, nh))
+            store_matrices(newcameramtx, distortion_vector, x)
+            print("STD Error:", get_error(objpoints, imgpoints, rvecs, tvecs, mtx, distortion_vector))
+    print(mtxs[0])
+    newcameramtx, roi=cv2.getOptimalNewCameraMatrix(mtxs[0], distortion_vectors[0], (w,h), 1, (nw, nh))
+    mapx, mapy = cv2.initUndistortRectifyMap(mtxs[0], distortion_vectors[0], None, newcameramtx, (nw, nh), 5)
     print("Showing the result")
+    print(newcameramtx)
     show_result(mapx, mapy)
